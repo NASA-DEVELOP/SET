2609de2807f10b1f270e31f7bd41acef68bfb3b9
Ryan Avery <rba.eos@gmail.com>
2017-02-16 17:57:31 -0500

    expanded English explanation of equations, goals, variables

--- testequationI.py
+++ testequationI.py
@@ -1,4 +1,10 @@
-# Our goal is to get a rough calculation of generalized artificial radiance using Dr. Ross's estimation of the light propogation functions, which are sketched out in Cinzano 2000 and Falchi 2016.
+# In the words of Cinzano 2000, here is what we are trying to calculate: 
+# "The light pollution propagation function f depends in general on the geometrical disposition (the altitude of the site and the area, and their mutual distance), on the atmospheric
+# distribution of molecules and aerosols and their optical characteristics in the chosen photometric band, on the shape of the emission function of the source, and on the direction 
+# of the sky observed." 
+
+# Our goal is to get a rough calculation of the illuminance per unit flux along the line of sight of an observer looking straight up (at the zenith), 
+# using Dr. Ross's estimation of the light propogation functions, which are sketched out in Cinzano 2000 and Falchi 2016.
 
 # We will first just work on translating these algebraic equations to python code for a given emission angle. We will be using a package called numpy, which has the capability to work with arrays. 
 # Arrays are rasters that can't be loaded up into geoprocessing software. They are quick to process and are a simple data format for data that occurs in a matrix.
@@ -7,8 +13,18 @@
 
 #Third, we will apply the integral equations over all "pixels" in the array using a nested for loop (for loop within a for loop).
 
-#Here are our equations
+# Here are our equations to describe upward light emission
 
 # I(Psi)_A = (1/(2*pi))*2*cos(Psi)      # This is the Lambertian circular distribution for stuff reflected from the ground
 # I(Psi)_B = (1/(2*pi))*0.554*Psi^4  # This is the horizon lighting model poorly shielded street lighting
 # I(Psi)_C = (1/(2*pi))*sin(3*Psi)       # This is the rose petal function that is max at 30 degrees. It is for the intermediate light
+
+#Before solving these equations we need to solve the equation for Psi
+# Psi = D/Rt(arcsin*(Rt*sin(D/Rt)/s))
+
+# To solve for Psi we need to solve for s
+# s = sqrroot(Rt^2+(Rt+Rs)^2-2*Rt*(Rt+Rs)*cos(D/Rt))
+# Where Rt is the average earth radius in kilometers (we can later get more specific to the local average earth radius near our study area)
+# Rs = h + Rt where h is the orbital height of NPP from the Earth's surface
+# D is the distance from the source to the site
+
