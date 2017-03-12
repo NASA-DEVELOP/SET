21cf9577c5fda565ba456699d17746e60049dd8f
Ryan <rba.eos@gmail.com>
2017-03-12 18:25:47 -0400

    finished making distance kernel. need to check VIIRS degree reolution since it doesn't come out to 750 meters

---
+++ test.py
@@ -0,0 +1,8 @@
+import numpy
+from numpy import *
+cent_lat = 43.9047
+radius = 6367.941
+x = 2*radius*arcsin(sqrt(sin((43.9047--155.95373823)/2)**2 + cos(-155.95373823)*cos(43.9047)*sin((-277.39075504-0)/2)**2))
+x = 2*radius*arcsin(sqrt(sin((43.9047--155.95373823)/2))**2)
+# x = ((cos(-155.95373823)*cos(43.9047)*sin((-277.39075504-0)/2)**2))
+print(x)
