a6d5fa8d66d6ccfd8dc32847effc5c7f3866fb80
stanley_yu <stanley.yu@columbia.edu>
2017-07-14 14:46:16 -0400

    added main function for generating maps and imported inputs

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ class SkyglowEstimationToolbox:
     def __init__(self, parent):
         self.parent = parent
         self.file_dialog = None
-        self.file = None
         self.img = None
+        self.lat_entry = None
+        self.ubr_entry = None
+        self.zen_entry = 0
+        self.azi_entry = 0
 
         # Sets window title and size on screen.
         self.parent.title("Skyglow Estimation Toolbox (SET)")
@@ -52,7 +55,7 @@ class SkyglowEstimationToolbox:
     def main_screen(self):
         # VIIRS Image Reference File
         file_label = Label(self.input_frame, text="Image File:", width=15, anchor=E)
-        self.file_dialog = Entry(self.input_frame, width=110, bd=2, relief=SUNKEN)
+        self.file_dialog = Entry(self.input_frame, width=109, bd=2, relief=SUNKEN)
         browse_button = Button(self.input_frame, text="Browse", command=self.import_file)
         file_label.grid(column=0, row=0)
         self.file_dialog.grid(column=1, columnspan=3, row=0)
@@ -61,30 +64,31 @@ class SkyglowEstimationToolbox:
         # Region Latitude (deg), Grand Teton National park = 43.7904 degrees N
         lat_label = Label(self.input_frame, text="Latitude (deg):", width=15, anchor=E)
         lat_label.grid(column=0, row=1)
-        lat_entry = Entry(self.input_frame, width=30, bd=2, relief=SUNKEN)
-        lat_entry.grid(column=1, row=1)
+        self.lat_entry = Entry(self.input_frame, width=30, bd=2, relief=SUNKEN)
+        self.lat_entry.grid(column=1, row=1)
 
         # Distance of Increasing Integration Increment (km), ubr, REF 2, Fig. 6, p.648
         ubr_label = Label(self.input_frame, text="Distance at which Integration Speed Increases"
                                                  " (km):", width=40, anchor=E)
         ubr_label.grid(column=2, row=1)
-        ubr_entry = Entry(self.input_frame, width=30, bd=2, relief=SUNKEN)
-        ubr_entry.grid(column=3, row=1)
+        self.ubr_entry = Entry(self.input_frame, width=30, bd=2, relief=SUNKEN)
+        self.ubr_entry.grid(column=3, row=1)
 
         # Zenith angle (deg), z, REF 2, Fig. 6, p.648
         zen_label = Label(self.input_frame, text="Zenith Angle (deg):", width=15, anchor=E)
         zen_label.grid(column=0, row=2)
-        zen_entry = Entry(self.input_frame, width=30, bd=2, relief=SUNKEN)
-        zen_entry.grid(column=1, row=2)
+        self.zen_entry = Entry(self.input_frame, width=30, bd=2, relief=SUNKEN)
+        self.zen_entry.grid(column=1, row=2)
 
         # Azimuth angle (deg)
         azi_label = Label(self.input_frame, text="Azimuth Angle (deg):", width=40, anchor=E)
         azi_label.grid(column=2, row=2)
-        azi_entry = Entry(self.input_frame, width=30, bd=2, relief=SUNKEN)
-        azi_entry.grid(column=3, row=2)
+        self.azi_entry = Entry(self.input_frame, width=30, bd=2, relief=SUNKEN)
+        self.azi_entry.grid(column=3, row=2)
 
         # Generate Artificial Skyglow Map
-        map_button = Button(self.input_frame, text="Generate Artificial Skyglow Map", width=93)
+        map_button = Button(self.input_frame, text="Generate Artificial Skyglow Map", width=93,
+                            command=self.generate_map)
         map_button.grid(column=1, columnspan=3, row=3)
     ##################################################
 
@@ -95,14 +99,26 @@ class SkyglowEstimationToolbox:
         file_name = file_name.show()
         self.file_dialog.insert(0, file_name)
         if file_name != '':
-            self.file = file_name
-            pilimg = Image.open(self.file)
+            pilimg = Image.open(file_name)
             canvas_size = (self.img_canvas.winfo_width(), self.img_canvas.winfo_height())
-            pilimgr = pilimg.resize(canvas_size, Image.ANTIALIAS)
-            self.img = ImageTk.PhotoImage(pilimgr)
+            pilimg_r = pilimg.resize(canvas_size, Image.ANTIALIAS)
+            # ### FIGURE OUT HOW TO MAKE TIFF DISPLAY MORE VISIBLE??? ###
+            # pilimg_alt = pilimg_r.convert("RGB", palette='WEB')
+            self.img = ImageTk.PhotoImage(pilimg_r)
             self.img_canvas.create_image(canvas_size[0]/2, canvas_size[1]/2, image=self.img)
         else:
             print('File is empty.')
+
+    # Generates artificial skyglow map based on VIIRS reference and local parameters.
+    def generate_map(self):
+        # Acquires input arguments.
+        lat_in = self.lat_entry.get()
+        ubr_in = self.ubr_entry.get()
+        zen_in = self.zen_entry.get()
+        azi_in = self.azi_entry.get()
+        # file_in = self.file_dialog.get()
+
+        print(lat_in, ubr_in, zen_in, azi_in)
 
 
 def main():
