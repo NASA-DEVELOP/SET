c5f1690e18c09ab29121a475043867b30fd9ba68
Maximilian Ioffe <maxioffe@gmail.com>
2018-06-18 17:31:17 -0400

    Delete pre-commit hook script

--- pre-commit.sh
+++
@@ -1,9 +0,0 @@
-STASH_NAME="pre-commit-$(date +%s)"
-git stash save -q --keep-index $STASH_NAME
-
-python -m unittest test
-
-STASHES=$(git stash list)
-if [[ $STASHES == "$STASH_NAME" ]]; then
-	git stash pop -q
-fi
